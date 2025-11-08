0d21390dfdd9c10cf778a9856fed35de48e24e1c
cyz199611 <b0145131h@gmail.com>
2025-11-07 20:57:43 -0500

    remove OPT

--- wkvaq_quant/longbench_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .scorer import scorer
-
-import json
-import os
-
-model2maxlen   = json.load(open(os.path.join(os.path.dirname(__file__), "config/model2maxlen.json"), "r"))
-dataset2prompt = json.load(open(os.path.join(os.path.dirname(__file__), "config/dataset2prompt.json"),"r"))
-dataset2maxlen = json.load(open(os.path.join(os.path.dirname(__file__), "config/dataset2maxlen.json"), "r"))
